246ae9517778d7fa62fca1a60294ab6fc4747bb2
Robin Duda <robin.duda@idainfront.se>
2018-12-08 01:12:25 +0100

    renamed method in receiver from cast to start.

--- web/js/receiver.js
+++ web/js/receiver.js
@@ -7,7 +7,7 @@ class Receiver {
      * Sets up the cast context and listens for a change to the ticker.
      * Also starts the websocket feed.
      */
-    cast() {
+    start() {
         this.namespace = "urn:x-cast:com.github.codingchili.coinbasefeed";
         this.context = cast.framework.CastReceiverContext.getInstance();
         this.player = this.context.getPlayerManager();
